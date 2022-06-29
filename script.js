9c5284f6c5fea95daa6196436b9c237f6de9e74a
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-28 17:08:20 -0700

    Created what will serve as my start page

---
+++ script.js
@@ -0,0 +1,8 @@
+///Create a Button that will start the quiz 
+//once Button Is clicked the button will dissapear 
+//User Will be prompted with the quiz questions 
+//Timer on the top right of the screen indicating time left
+//questions have to = a set amount of points totaling 100.
+//if user gets questions wrong the don't receive points for the question + question is skipped.
+//Scores are to be stored and displayed for reference
+
