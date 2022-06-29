bdb78c484e03c28738e9b6f3d18975319405ba58
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-28 21:09:53 -0700

    Wrapped everything up once Linked to my questions.js file it should work as intended

--- script.js
+++ script.js
@@ -109,3 +109,70 @@ let Scores = scoresArray(storedArray, emptyArray);
   total.append(startingInput);
   total.append(endInput);
 }
+
+const Save = (array) => {
+    window.localStorage.setItem("highScores", JSON.stringify(array));
+  }
+  
+  const scoresArray = (arr1, arr2) => {
+    if(arr1 !== null) {
+      return arr1
+    } else {
+      return arr2
+    }
+  }
+  
+  const removeEls = (...els) => {
+    for (let ment of els) ment.remove();
+  }
+  
+  function showAllScores() {
+    removeEls(numTime, quizStart, total);
+    let Scores = scoresArray(storedArray, emptyArray);
+  
+    Scores.forEach(obj => {
+      let nameInitial = obj.nameInitial;
+      let storedScore = obj.score;
+      let resultsP = document.createElement("p");
+      resultsP.innerText = `${nameInitial}: ${storedScore}`;
+      divScore.append(resultsP);
+    });
+  }
+  
+  function viewScores() {
+    viewScoresBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      removeEls(numTime, quizStart);
+      showAllScores();
+      removeEls(viewScoresBtn);
+      removeScores();
+      buttonReturn();
+    });
+  }
+  
+  function removeScores() {    
+    let clearBtn = document.createElement("input");
+    clearBtn.setAttribute("type", "button");
+    clearBtn.setAttribute("value", "Clear Scores");
+    clearBtn.addEventListener("click", function(event){
+      event.preventDefault();
+      removeEls(divScore);
+      window.localStorage.removeItem("highScores");
+    })
+    divScore.append(clearBtn)
+  }
+  
+  function buttonReturn() {
+    let backBtn = document.createElement("input");
+    backBtn.setAttribute("type", "button");
+    backBtn.setAttribute("value", "Go Back");
+    backBtn.addEventListener("click", function(event){
+      event.preventDefault();
+      window.location.reload();
+    })
+    buttonsDiv.append(backBtn)
+  }
+  
+  
+  viewScores();
+  
