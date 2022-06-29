9e2e5998a39d96a89294d1a470142dc6071cef81
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-28 18:26:22 -0700

    Got an idea as to how the page is gonna work and changed a few things to go with my idea

--- script.js
+++ script.js
@@ -5,4 +5,15 @@
 //questions have to = a set amount of points totaling 100.
 //if user gets questions wrong the don't receive points for the question + question is skipped.
 //Scores are to be stored and displayed for reference
+//will be using the let function in order to set values 
+
+let numTime = document.getElementById("numTime")
+let scoresBtn = document.getElementById("all-scores")
+//Keeps track as to what question # it is
+var numquestions = 0;
+//keeps track of time
+let timeleft = 76;
+//keeps track of points 
+let points = 0
+
 
