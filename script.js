34466c03b6145bba282d3dfbe05ef2c726906299
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-28 19:06:44 -0700

    Button clicked will start timer

--- script.js
+++ script.js
@@ -7,7 +7,12 @@
 //Scores are to be stored and displayed for reference
 //will be using the let function in order to set values 
 
+
+let total = document.getElementById("total")
+let divscore = document.getElementById("d-Scores")
+//will display time
 let numTime = document.getElementById("numTime")
+//will display scores
 let scoresBtn = document.getElementById("all-scores")
 //Keeps track as to what question # it is
 var numquestions = 0;
@@ -15,5 +20,39 @@ var numquestions = 0;
 let timeleft = 76;
 //keeps track of points 
 let points = 0
+
+let quizStart =  document.getElementById("Begin");
+quizStart.addEventListener("click",showTime);
+
+
+let highscore = [];
+
+
+let storedArray = JSON.parse(window.localStorage.getItem("HighScores"));
+
+
+function showTime() {
+    showQuestions();
+   let timeInterval = setInterval(function() {
+    timeLeft--;
+    numTime.textContent = "";
+    numTime.textContent - "Time: " + timeleft;
+    if (timeleft <= 0 || numquestions === questions.lenght){
+        clearInterval(timeInterval);
+        storeScore();
+    }
+    }, 1000);
+   }
+
+   function showQuestions(){
+       ElsRemove(quizStart);
+
+    if(numquestions < questions.lenght){
+        divQ
+    }
+   }
+
+
+
 
 
